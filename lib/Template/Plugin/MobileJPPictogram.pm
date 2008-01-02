099eaebdc9fbd69791175976b97a57b256a2e9a5
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-01-02 04:40:55 +0000

    spec changes
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Template-Plugin-MobileJPPictogram/trunk@3965 d0d07461-0603-4401-acd4-de1884942a52

--- lib/Template/Plugin/MobileJPPictogram.pm
+++ lib/Template/Plugin/MobileJPPictogram.pm
@@ -10,44 +10,85 @@ use base qw(Template::Plugin);
 use Encode::JP::Mobile ':props';
 use Encode::JP::Mobile::Charnames;
 
-our $FILTER_NAME = 'escape_pictograms';
-
 sub new {
     my ( $self, $context, @args ) = @_;
-    my $format = $args[0] || '[%s]';
-    my $name = $args[1] || $FILTER_NAME;
-    $context->define_filter( $name, sub { commify($format, $_[0]) }, 0 );
+    $context->define_filter( 'pictogram_charname', \&_charname, 1 );
+    $context->define_filter( 'pictogram_unicode',  \&_unicode,  1 );
     return $self;
 }
 
-sub commify {
-    my $format = shift;
-    local $_ = shift;
+sub _charname {
+    my ( $ctx, $unicode ) = @_;
+    die "unicode string missing for pictogram_charname" unless $unicode;
 
-    s{(\p{InMobileJPPictograms})}{
-        my $name = Encode::JP::Mobile::Charnames::unicode2name(unpack 'U*', $1);
-        sprintf $format, $name;
-    }ge;
+    sub {
+        local $_ = shift;
 
-    return $_;
+        s{(\p{InMobileJPPictograms})}{
+            my $name = Encode::JP::Mobile::Charnames::unicode2name(unpack 'U*', $1);
+            sprintf $unicode, $name;
+        }ge;
+
+        $_;
+    };
+}
+
+sub _unicode {
+    my ( $ctx, $unicode ) = @_;
+    die "unicode string missing for pictogram_unicode" unless $unicode;
+
+    sub {
+        local $_ = shift;
+
+        s{(\p{InMobileJPPictograms})}{
+            sprintf $unicode, unpack 'U*', $1;
+        }ge;
+
+        $_;
+    };
 }
 
 1;
 __END__
 
-=for stopwords aaaatttt dotottto gmail
+=for stopwords aaaatttt dotottto gmail pictogram pictograms Unicode charnames
 
 =head1 NAME
 
-Template::Plugin::MobileJPPictogram -
+Template::Plugin::MobileJPPictogram - Japanese mobile phone's pictogram operator
 
 =head1 SYNOPSIS
 
-  use Template::Plugin::MobileJPPictogram;
+  # controller
+  my $tt = Template->new;
+  $tt->process('foo.tt', {body => "\x{E001}"});
+
+  # foo.tt
+  [% USE MobileJPPictogram %]
+  [% body | pictogram_charname('***%s***') %]
+  [% body | pictogram_unicode('<img src="/img/pictogram/%X.gif" />') %]
+
+  # output
+  ***男の子***
+  <img src="/img/pictogram/E001.gif" />
 
 =head1 DESCRIPTION
 
-Template::Plugin::MobileJPPictogram is
+Template::Plugin::MobileJPPictogram is Japanese mobile phone's pictogram operator.
+
+=head1 FILTERS
+
+=head2 pictogram_charname
+
+format with charnames.
+
+    [% body | pictogram_charname('***%s***') %]
+
+=head2 pictogram_unicode
+
+format with Unicode.
+
+    [% body | pictogram_unicode('<img src="/img/pictogram/%X.gif" />') %]
 
 =head1 AUTHOR
 
@@ -60,4 +101,3 @@ Tokuhiro Matsuno E<lt>tokuhirom aaaatttt gmail dotottto commmmmE<gt>
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
-=cut
--- t/01_simple.t
+++ t/01_simple.t
@@ -5,6 +5,8 @@ use Test::Base;
 use Template;
 use Encode;
 
+# 鬱膣愛味噌
+
 filters {
     input => [qw/yaml escape/],
 };
@@ -28,7 +30,23 @@ __END__
 
 ===
 --- input
-template: "[% USE MobileJPPictogram %][% x | escape_pictograms %]"
-x: PICT:\x{E001}
---- expected: PICT:[男の子]
+template: "[% USE MobileJPPictogram %][% x | pictogram_charname('***%s***') %]"
+x: PICT:\x{E754}
+--- expected: PICT:***ウマ***
+
+===
+--- input
+template: |+
+    [% USE MobileJPPictogram %][% x | pictogram_unicode('<img src="/img/pictogram/%04X.gif" />') %]
+x: PICT:\x{E754}
+--- expected
+PICT:<img src="/img/pictogram/E754.gif" />
+
+===
+--- input
+template: |+
+    [% USE MobileJPPictogram %][% x | pictogram_unicode('<img src="/img/pictogram/%d.gif" />') %]
+x: PICT:\x{E754}
+--- expected
+PICT:<img src="/img/pictogram/59220.gif" />
 
