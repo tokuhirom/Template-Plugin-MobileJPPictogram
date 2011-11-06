a697586d864301b4520c5865a1834777a2cf7e92
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-11-06 21:51:06 +0900

    Checking in changes prior to tagging of version 0.04.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index f65cd55..b19b123 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension Template::Plugin::MobileJPPictogram
    
    +0.04
    +
    +    - Test::Base is required
    +
     0.03
    
         - modernize package

--- lib/Template/Plugin/MobileJPPictogram.pm
+++ lib/Template/Plugin/MobileJPPictogram.pm
@@ -3,7 +3,7 @@ package Template::Plugin::MobileJPPictogram;
 use strict;
 use warnings;
 use 5.0080008;
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 require Template::Plugin;
 use base qw(Template::Plugin);
